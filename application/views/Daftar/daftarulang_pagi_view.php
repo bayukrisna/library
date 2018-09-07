2586d45a139720a0ea3cd4b90874e907341f3eb6
aldirahmanws <rahmanaldi290@gmail.com>
2018-09-07 13:23:56 +0700

    s
    s

--- application/views/Daftar/daftarulang_pagi_view.php
+++ application/views/Daftar/daftarulang_pagi_view.php
@@ -14,7 +14,7 @@
                 <div class="form-group">
                   <label for="no">No. Registrasi</label>
                   <input type="text" name="id_du" class="form-control" id="id_du" placeholder=""  .input-sm value="<?php echo $du_pagi->id_du; ?>" readonly>
-                   <input type="text" name="id_mahasiswa" class="form-control" id="id_mahasiswa" placeholder=""  .input-sm value="<?php echo $kodeunik_mhs; ?>" readonly>
+                   <input type="hidden" name="id_mahasiswa" class="form-control" id="id_mahasiswa" placeholder=""  .input-sm value="<?php echo $kodeunik_mhs; ?>" readonly>
                 </div>
              
                 <div class="form-group">
