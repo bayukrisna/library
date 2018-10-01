ea4aecf743c1d7e5681f3cb4df5a602348d61a06
bayukrisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2018-10-01 11:20:10 +0700

    wew

--- application/views/data_sekolah.php
+++
@@ -1,6 +0,0 @@
-<?php 
-  foreach($getPreschool as $row)
-  { 
-    echo '<option value="'.$row->id_sekolah.'">'.$row->nama_sekolah.'</option>';
-  }
-?>
--- application/views/ss.php
+++
@@ -1,2 +0,0 @@
-
-<iframe src="https://calendar.google.com/calendar/embed?src=faisalraharja990%40gmail.com&ctz=Asia%2FJakarta" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
