0ecaf5b9614a78076cb5698f681f106bef29911c
bayukrisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2018-10-01 11:28:47 +0700

    routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'landing';
-$route['404_override'] = 'landing';
+$route['default_controller'] = 'Lahan';
+$route['404_override'] = 'Lahan';
 $route['translate_uri_dashes'] = FALSE;
